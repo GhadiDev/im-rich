cad3ac2702807b6d41fd3a9b7806a7c255d2875b
Ghadi <ghadimaz@gmail.com>
2022-08-12 10:53:46 +0300

    upload project

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by  Ghadi on 05/12/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
